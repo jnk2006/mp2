399ccb38799605b363ebb959a387fe1f7d441723
jnk2006 <jnk22@njit.edu>
2018-11-02 12:21:12 -0400

    8. Created a controller, route, and view to show an about page and add it to the main menu of the site.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,11 @@ class HomeController extends Controller
         return view('home');
     }
 
+    public function about()
+    {
+        return view('about');
+    }
+
     public function contact()
     {
         return view('contact');
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/about', 'HomeController@about')->name('about');
+
 Route::get('/contact', 'HomeController@contact')->name('contact');
 
 Route::post('/contact', 'HomeController@store')->name('contact.store');
