c907c934ef7433abc593dd25ea62e57e27b5cbc6
jnk2006 <jnk22@njit.edu>
2018-11-02 12:11:08 -0400

    7. Created a controller, route, and view to show a contact form and add it to the main menu of the site.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,20 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    function store(Request $request)
+    {
+        $name = $request -> name;
+        return redirect()->route('thanks',['name' => $name]);
+    }
+
+    function thanks($name, Request $request)
+    {
+        return view('thankyou')->with(compact('name'));
+    }
 }
